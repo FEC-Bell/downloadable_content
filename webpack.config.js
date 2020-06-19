d7372add7f47615e3f6f4d27b47ee20c266bcd34
Jeremy Chen <jeremylfchen@gmail.com>
2020-06-19 11:49:43 -0700

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+            sourceMaps: "inline"
+          }
+        }
+      }
+    ]
+  }
+
+}
