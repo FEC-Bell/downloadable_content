aed5e51a8ccab2477728dac631e271a4d0a47cef
Jeremy Chen <jeremylfchen@gmail.com>
2020-07-28 20:33:58 -0700

    removed moment dependancy

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const compression =  require('compression');
 const db = require('../database/index.js');
 
 
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(compression());
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/app/:gameid', (req, res) => {
